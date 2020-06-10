6edb105e27151944b04ffbb37d27ae8f47e6f318
tainzhi <qfq61@qq.com>
2020-06-10 13:20:25 +0800

    add common classes

---
+++ src/main/java/com/tainzhi/android/common/Constant.kt
@@ -0,0 +1,10 @@
+package com.tainzhi.android.common
+
+/**
+ * @author:      tainzhi
+ * @mail:        qfq61@qq.com
+ * @date:        2020/6/10 11:00
+ * @description:
+ **/
+ 
+var isLoggable = true
---
+++ src/main/java/com/tainzhi/android/common/base/Response.kt
@@ -0,0 +1,12 @@
+package com.tainzhi.android.common.base
+
+import java.io.Serializable
+
+/**
+ * @author:       tainzhi
+ * @mail:         qfq61@qq.com
+ * @date:         2020/1/19 下午9:18
+ * @description:
+ **/
+
+data class Response<out T>(val errorCode: Int, val errorMsg: String, val data: T) : Serializable
