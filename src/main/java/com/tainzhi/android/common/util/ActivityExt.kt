4d5985e655145d3b8c5955a0199c9e76946d280f
tainzhi <qfq61@qq.com>
2021-01-04 21:57:38 +0800

    add activity ext function

--- src/main/java/com/tainzhi/android/common/util/ActivityExt.kt
+++ src/main/java/com/tainzhi/android/common/util/ActivityExt.kt
@@ -178,3 +178,16 @@ fun Context.isForeGround(): Boolean {
     return false
 }
 
+fun Activity.hideSystemUiAndNavigation() {
+    val decorView: View = this.window.decorView
+    decorView.systemUiVisibility =
+            (View.SYSTEM_UI_FLAG_IMMERSIVE
+                    // Set the content to appear under the system bars so that the
+                    // content doesn't resize when the system bars hide and show.
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN // Hide the nav bar and status bar
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+}
+
